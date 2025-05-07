ee58ac7e076f49375373777642eca03d95dc6f21
JosuneRG <josunerg94@gmail.com>
2025-05-07 18:42:08 +0200

    cambios

---
+++ tests/main.spec.js
@@ -0,0 +1,8 @@
+describe('isIsogram kata', () => {
+    it('should exist a variable named isIsogram', () => {
+        // Arrange
+        // Act
+        // Assert
+        expect(isIsogram).not.toBe(undefined)
+    })
+})
